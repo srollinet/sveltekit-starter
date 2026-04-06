487badc79673006f827e163a7b5bce16e0e7a546
Sylvain <sylvain.rollinet@gmail.com>
2026-04-06 09:48:07 +0000

    Make tests pass

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  webServer: { command: 'pnpm build && pnpm preview', port: 4173 },
+  webServer: { command: 'pnpm build && pnpm preview --mode test', port: 4173 },
   testMatch: '**/*.e2e.{ts,js}',
 });
