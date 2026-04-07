3996b86f1df07d1a0f8f7e38752a184fbd0b0797
Sylvain <sylvain.rollinet@gmail.com>
2026-04-07 16:25:45 +0000

    Manual adjustements

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   schema: './src/lib/server/db/schema/*',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url:
+      process.env.DATABASE_URL ??
+      (() => {
+        throw new Error('DATABASE_URL is not set');
+      })(),
   },
 });
