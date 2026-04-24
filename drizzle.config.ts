a1c7632920eeb83277a5720494a9d8825903b299
Sylvain <sylvain.rollinet@gmail.com>
2026-04-24 10:18:18 +0200

    Fix knip

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     url:
       process.env.DATABASE_URL ??
       (() => {
-        throw new Error('DATABASE_URL is required');
+        console.error('DATABASE_URL is required');
+        return '';
       })(),
   },
 });
